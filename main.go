42e4cf7e7240ca003c571f2b919d1bf9e24d711a
Camilo Rivas <crivas.folch@gmail.com>
2024-05-25 17:24:29 +0200

    feat: add mux

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("hello world")
+	headers := handlers.AllowedHeaders([]string{"Contety-Type", "Access-Control-Allow-Headers", "Authorization"})
+	methods := handlers.AllowedMethods([]string{"DELETE", "POST", "GET", "OPTIONS", "PUT", "PATCH"})
+	origins := handlers.AllowedOrigins([]string{"*"})
+
+	loggedRouter := handlers.LoggingHandler(os.Stdout, mux.NewRouter())
+
+	log.Fatal(http.ListenAndServe(":8081", handlers.CORS(headers, methods, origins)(loggedRouter)))
 }
