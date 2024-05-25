d2712be23602e982f78afbcaf71fd64ee8814bf8
Camilo Rivas <crivas.folch@gmail.com>
2024-05-25 16:28:51 +0200

    feat: get quiz use case

---
+++ core/repository/quiz.go
@@ -0,0 +1,7 @@
+package repository
+
+import "github.com/cirivas/challenge-quiz/core/entities"
+
+type QuizRepository interface {
+	GetQuiz(id string) (entities.Quiz, error)
+}
---
+++ core/use_cases/get_quiz/get_quiz.go
@@ -0,0 +1,22 @@
+package use_cases
+
+import (
+	"github.com/cirivas/challenge-quiz/core/entities"
+	"github.com/cirivas/challenge-quiz/core/repository"
+)
+
+type getQuiz struct {
+	quizRepository repository.QuizRepository
+}
+
+type GetQuizUseCase interface {
+	GetQuiz(quizId string) (entities.Quiz, error)
+}
+
+func NewGetQuizUseCase(r repository.QuizRepository) GetQuizUseCase {
+	return &getQuiz{r}
+}
+
+func (uc *getQuiz) GetQuiz(quizId string) (entities.Quiz, error) {
+	return uc.quizRepository.GetQuiz(quizId)
+}
