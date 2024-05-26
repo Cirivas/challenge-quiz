d48708ae1f2589d7392e18bfce8cd59475ed078f
Camilo Rivas <crivas.folch@gmail.com>
2024-05-26 18:49:27 +0200

    feat: add new repository

---
+++ core/repository/score.go
@@ -0,0 +1,9 @@
+package repository
+
+type ScoreRepository interface {
+	SaveScore(respondent string, score int) error
+	// Return the score of 'respondent' for 'quizId'
+	GetScore(respondent string, quizId string) (int, error)
+	// Returns the score of everyone except 'respondent' for 'quizId'
+	GetOthersScore(respondent string, quizId string) ([]int, error)
+}
