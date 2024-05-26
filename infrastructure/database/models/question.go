7162ecee384fa188805f895426eda8720f4e0c79
Camilo Rivas <crivas.folch@gmail.com>
2024-05-26 17:10:06 +0200

    fix: missing field

--- infrastructure/database/models/question.go
+++ infrastructure/database/models/question.go
@@ -3,8 +3,7 @@ package models
 import "github.com/cirivas/challenge-quiz/core/entities"
 
 type Question struct {
-	Id            string                        `json:"id"`
 	Text          string                        `json:"text"`
 	Alternatives  map[entities.AnswerKey]string `json:"alternatives"`
-	CorrectAnswer entities.AnswerKey            `json:"-"`
+	CorrectAnswer entities.AnswerKey            `json:"correctAnswer"`
 }
